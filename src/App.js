97a2b2fb9d73376afdcd110e6483016e18999c76
Superkid <kevinchristensen76923@gmail.com>
2025-02-22 14:37:58 +0700

    basic add and remove job

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [job, setJob] = useState("");
+	const [jobs, setJobs] = useState(() => {
+		const storageJobs = JSON.parse(localStorage.getItem("jobs"));
+
+		return storageJobs ?? [];
+	});
+
+	const handleSubmit = () => {
+		if (job === "") {
+			alert("Please enter a job");
+			return [...jobs];
+		}
+		setJobs((prev) => {
+			const newJobs = [...prev, job];
+
+			const jsonJobs = JSON.stringify(newJobs);
+			localStorage.setItem("jobs", jsonJobs);
+
+			return newJobs;
+		});
+		setJob("");
+	};
+
+	const handleRemove = (index) => {
+		setJobs((prev) => {
+			const newJobs = jobs.filter((job) => job !== index);
+			const jsonJobs = JSON.stringify(newJobs);
+
+			localStorage.setItem("jobs", jsonJobs);
+
+			return newJobs;
+		});
+	};
+
+	return (
+		<div className="App">
+			<input value={job} onChange={(e) => setJob(e.target.value)} />
+			<button onClick={handleSubmit}>Submit</button>
+			<ul>
+				{jobs.map((job, index) => (
+					<li key={index}>
+						{job}
+						<button onClick={() => handleRemove(job)}>
+							Remove
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
